55e27286543da642849ce4eace323bb06d7a9a2a
Han Kim <hanwkim88@gmail.com>
2023-04-18 17:15:17 -0400

    Initial commit

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import './Home.scss';
+
+export default function Home() {
+    return (
+        <main className="home">
+            <section className="header">
+                
+            </section>
+        </main>
+    )
+}
