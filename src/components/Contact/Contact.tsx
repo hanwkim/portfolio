28705d0fca19de6c135e437628bf74f0c117cf73
Han Kim <hanwkim88@gmail.com>
2023-05-07 16:45:54 -0400

    Add contact icons

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,10 +1,23 @@
 import './Contact.scss';
+import githubIcon from '../../assets/icons/github.svg';
+import linkedinIcon from '../../assets/icons/linkedin.svg';
+import mailIcon from '../../assets/icons/mail.svg';
 
 export default function Contact() {
     return (
         <section className="contact">
             <h2 className="contact__title">Get In Touch</h2>
-            <div className="contact__image-container"></div>
+            <div className="contact__image-container">
+                <a className="contact__link" href="https://linkedin.com">
+                    <img className="contact__image" src={linkedinIcon} alt="LinkedIn Icon"></img>
+                </a>
+                <a className="contact__link" href="https://github.com">
+                    <img className="contact__image" src={githubIcon} alt="github icon"></img>
+                </a>
+                <a className="contact__link" href="https://gmail.com">
+                    <img className="contact__image" src={mailIcon} alt="Email icon"></img>
+                </a>
+            </div>
         </section>
     )
 }
