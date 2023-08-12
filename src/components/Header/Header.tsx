4346b12b6bdb1c0556d40705307ed77bbf208b09
Han Kim <hanwkim88@gmail.com>
2023-08-11 21:01:41 -0400

    Add styling and spacing for tablet and desktop sizes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,20 +3,22 @@ import "./Header.scss";
 export default function Header() {
 	return (
 		<header className="header">
-			<nav className="header__links">
-				<a className="header__link" href="#hero">
-					Home
-				</a>
-				<a className="header__link" href="#about">
-					About
-				</a>
-				<a className="header__link" href="#projects">
-					Projects
-				</a>
-				<a className="header__link" href="#contact">
-					Contact
-				</a>
-			</nav>
+			<div className="header__container">
+				<nav className="header__links">
+					<a className="header__link" href="#hero">
+						Home
+					</a>
+					<a className="header__link" href="#about">
+						About
+					</a>
+					<a className="header__link" href="#projects">
+						Projects
+					</a>
+					<a className="header__link" href="#contact">
+						Contact
+					</a>
+				</nav>
+			</div>
 		</header>
 	);
 }
